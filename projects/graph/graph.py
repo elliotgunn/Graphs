5ec1be9f16dd55e04e41cbbfa503ca75a0dc2e2a
elliotgunn <elliot.j.gunn@gmail.com>
2020-03-19 15:52:48 +0000

    graph.py

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -211,7 +211,7 @@ class Graph:
                         # stack them on the stack to be visited
                         stack.push(path_copy)
 
-    def dfs_recursive(self, starting_vertex):
+    def dfs_recursive(self, starting_vertex, destination_vertex, visited=None, path=None):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
@@ -221,8 +221,10 @@ class Graph:
         """
         if visited is None:
             visited = set()
+
         if path is None:
             path = []
+
         if starting_vertex not in visited:
             visited.add(starting_vertex)
             path_copy = path.copy()
@@ -233,6 +235,7 @@ class Graph:
                 new_path = self.dfs_recursive(neighbor, destination_vertex, visited, path_copy)
                 if new_path is not None:
                     return new_path
+
         return None
 
 
