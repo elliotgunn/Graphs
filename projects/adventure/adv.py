74cb02df5e74fca1731ccbb2864c330fa5b08865
elliotgunn <elliot.j.gunn@gmail.com>
2020-03-20 12:19:27 +0000

    start sc

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,20 +1,20 @@
 from room import Room
 from player import Player
 from world import World
-
+from util import Stack, Queue
 import random
 from ast import literal_eval
 
 # Load world
+#------------------------------------------------------------------------------#
 world = World()
 
-
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -24,10 +24,22 @@ world.load_graph(room_graph)
 world.print_rooms()
 
 player = Player(world.starting_room)
+#------------------------------------------------------------------------------#
+
+# function 1:
+
+def
+
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+
+
+
+
+
+
 
 
 
